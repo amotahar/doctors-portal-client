843d443cebcab37ebd92f753c076e87ee247f171
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-02-13 22:10:10 +0600

    Update Footer.js
    Gsjs

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,12 @@ import React from 'react'
 function Footer() {
     return (
         <div>
-            <h2>This is Footer</h2>
+        <h2>This is Footer</h2>
+        <h2>This is Footer</h2>
+        <h2>This is Footer</h2>
+        <h2>This is Footer</h2>
+        <h2>This is Footer</h2>
+        
         </div>
     )
 }
