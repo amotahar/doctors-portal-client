09e8c8a229fb06ed2bd25577fce6e8815d0f5eab
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-02-19 21:43:47 +0600

    Update Home.js
    gj

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -4,6 +4,12 @@ function Home() {
     return (
         <div className='mx-5'>
             <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
         </div>
     )
 }
