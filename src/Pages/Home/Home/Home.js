9b561520a673438d103e3085765870afc92001c3
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-02-21 19:32:57 +0600

    Update Home.js
    gjj

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -7,6 +7,11 @@ function Home() {
         
         <h2>This is home</h2>
         
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
         
         <h2>This is home</h2>
         <h2>This is home</h2>
