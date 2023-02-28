01359ef9fda4126e831282d8088c7955e4775287
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-02-28 23:21:39 +0600

    Update Home.js
    Gajsjv

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -17,6 +17,22 @@ function Home() {
         
         
         <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        
+        <h2>This is home</h2>
         
         
         <h2>This is home</h2>
