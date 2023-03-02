fd70660233895cbd9f08dcd0491101649bff761a
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-02 06:14:59 +0600

    Update Home.js
    Gagsgs

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -15,6 +15,10 @@ function Home() {
         
         <h2>This is home</h2>
         
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        
         
         <h2>This is home</h2>
         <h2>This is home</h2>
