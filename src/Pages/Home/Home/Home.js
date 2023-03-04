783e55a7663cc15048c6f438e6b25f1b5142277f
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-04 14:52:13 +0600

    Update Home.js
    Afhagsb

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -12,6 +12,19 @@ function Home() {
         
         <h2>This is home</h2>
         <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
         
         <h2>This is home</h2>
         
