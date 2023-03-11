e82616c50055c0f1783a8b074c9e10a9104cf13f
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-11 06:08:47 +0600

    Update Home.js
    Gkukcmgh

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -16,6 +16,14 @@ function Home() {
         <h2>This is home</h2>
         <h2>This is home</h2>
         <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
         
         <h2>This is home</h2>
         
