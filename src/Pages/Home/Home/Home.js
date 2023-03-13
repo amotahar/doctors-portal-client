c3b58ce7c407232e5128db9f011f75a5448f2e48
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-13 15:35:21 +0600

    Update Home.js
    dhhdkj

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -23,6 +23,21 @@ function Home() {
         <h2>This is home</h2>
         <h2>This is home</h2>
         <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
         
         <h2>This is home</h2>
         <h2>This is home</h2>
