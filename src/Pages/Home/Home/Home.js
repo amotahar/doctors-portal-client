8484cbb70aeac0f6aabfbd8c54d039734b85334f
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-14 17:20:16 +0600

    Update Home.js
    Afjagsjdjdb

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -8,6 +8,19 @@ function Home() {
         <h2>This is home</h2>
         
         <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        
+        <h2>This is home</h2>
         
         <h2>This is home</h2>
         <h2>This is home</h2>
