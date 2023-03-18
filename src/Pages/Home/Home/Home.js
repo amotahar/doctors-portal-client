1260fc0866423874fe713e64d1217cef9ea8326a
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-18 15:56:22 +0600

    Update Home.js
    ahsjsbvd

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -145,6 +145,15 @@ function Home() {
         <h2>This is home</h2>
         <h2>This is home</h2>
         <h2>This is home</h2>
+        
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
+        <h2>This is home</h2>
         <h2>This is home</h2>
         <h2>This is home</h2>
         <h2>This is home</h2>
