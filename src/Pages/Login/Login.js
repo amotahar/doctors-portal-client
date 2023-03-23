2f285c41eeaa323ac1c3cd5ea8502c1cc7567ecc
Motahar Hossain <motaharhossain.cse@gmail.com>
2023-03-23 13:35:17 +0600

    Update Login.js
    done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,87 +2,9 @@ import React from 'react'
 
 function Login() {
     return (
-        <div>
-            
+        <div>        
 
-
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        
-        
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        
-        
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        
-        
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
-        <h2>This is log in</h2>
+        <h2>This is log in</h2>   
         
         </div>
     )
